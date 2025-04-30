0d75452fe0d3366beba5284b04d50f0134593653
alexito0307 <alexito030710@gmail.com>
2025-04-30 13:47:26 -0600

    feat: :sparkles: Added new usercontroller

---
+++ backend/controllers/usersControllers.js
@@ -0,0 +1,29 @@
+const jwt = require('jwt')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require("express-async-handler");
+const User = require('../models/usersModel');
+
+const login = asyncHandler( async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({email})
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json(
+      {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generarToken(user.id)
+      }
+    )
+  } else {
+    res.status(400);
+    throw new Error ("Credenciales incorrectas")
+  }
+})
+
+const generarToken = (id) = () => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30m'
+  })
+}
